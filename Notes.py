55514485f64aa0536a10597e3db717186a8b4a73
anyquu <expendead@gmail.com>
2023-03-24 19:06:50 +0300

    Notes.Ver1
    Now its will be save to PlaceHolderText

--- Notes.py
+++ Notes.py
@@ -12,6 +12,8 @@ class Notes(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.init_ui()
         self.ui.pushButton.clicked.connect(self.save1)
+        self.ui.pushButton_2.clicked.connect(self.save2)
+        self.ui.pushButton_3.clicked.connect(self.save3)
 
 
 
@@ -22,9 +24,15 @@ class Notes(QtWidgets.QMainWindow):
         self.ui.text3.setPlaceholderText('Note 3')
 
     def save1(self):
-        self.ui.text1.setPlaceholderText(self.note1)
+        self.text1 = self.ui.text1.toPlainText()
+        self.ui.text1.setPlaceholderText(self.text1)
 
-        
+    def save2(self):
+        self.text2 = self.ui.text2.toPlainText()
+        self.ui.text2.setPlaceholderText(self.text2)
+    def save3(self):
+        self.text3 = self.ui.text3.toPlainText()
+        self.ui.text3.setPlaceholderText(self.text3)
 
 app = QtWidgets.QApplication([])
 application = Notes()
